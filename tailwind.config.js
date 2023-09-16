b772783db2cec3265277e607e909ac54525647f9
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-09-16 17:08:52 +0600

    Declare a new custom variable for text medium light color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ export default {
     extend: {
       colors: {
         textPrimary: "#1C1B1B",
+        textMediumLight: "#1c1b1bcc",
         textLight: "#1c1b1b99",
         primary: "#2F80ED",
         primaryDark: "#2a73d5",
         danger: "#CB0B0B",
+        lightBorder: "#1c1b1b33",
       },
       borderRadius: {
         default: "8px",
